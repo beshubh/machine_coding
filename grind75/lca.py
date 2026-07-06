d30ccccccd0f074215b2585c456bf935c9052d00
beshubh <be.shubh42@gmail.com>
2026-07-06 21:48:30 +0530

    lowest common ancestory of BST

---
+++ grind75/lca.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+
+        def inner(root):
+            if p.val < root.val and q.val < root.val:
+                return inner(root.left)
+            if p.val > root.val and q.val > root.val:
+                return inner(root.right)
+            return root
+
+        return inner(root)
