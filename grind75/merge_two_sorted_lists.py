1f287f365b72e324f0b74007914eb062707c7972
beshubh <be.shubh42@gmail.com>
2026-07-06 20:19:18 +0530

    merge two sorted lists

--- grind75/merge_two_sorted_lists.py
+++ grind75/merge_two_sorted_lists.py
@@ -71,8 +71,10 @@ class Solution2:
         dummy = ListNode()
         if list1.val <= list2.val:
             dummy.next = list1
+            list1 = list1.next
         else:
             dummy.next = list2
+            list2 = list2.next
         current = dummy.next
         while list1 and list2:
             if list1.val <= list2.val:
