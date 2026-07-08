80afb165452519d2eb532fb64c8941a4ec68b04c
beshubh <be.shubh42@gmail.com>
2026-07-08 21:38:49 +0530

    longest palindrome

---
+++ grind75/longest_palindrome.py
@@ -0,0 +1,12 @@
+import collections
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        counter = collections.Counter(s)
+        result = 0
+        for ch in set(s):
+            if counter[ch] % 2 == 0:
+                result += counter[ch]
+            else:
+                result += counter[ch] - 1
