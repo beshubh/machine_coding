691daae82d739224fec83189f1c7455a293c13a4
beshubh <be.shubh42@gmail.com>
2026-07-10 09:30:56 +0530

    roman to integer optimized

--- grind75/roman_to_integer.py
+++ grind75/roman_to_integer.py
@@ -3,16 +3,17 @@ MAP = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        stack = []
-        for i, ch in enumerate(reversed(s)):
+        answer = 0
+        for i in reversed(range(len(s))):
+            ch = s[i]
             if ch not in MAP:
                 raise ValueError(f"No such roman: {ch}")
             num = MAP[ch]
-            if stack:
-                if num < stack[-1]:
-                    stack.append(-num)
+            if i != len(s) - 1:
+                if num < MAP[s[i + 1]]:
+                    answer -= num
                 else:
-                    stack.append(num)
+                    answer += num
             else:
-                stack.append(num)
-        return sum(stack)
+                answer += num
+        return answer
